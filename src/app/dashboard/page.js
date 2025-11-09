76c59d0eb54659fd60ce7a7f878972c1d99f8011
richurichu <paulsonrichu@gmail.com>
2025-11-09 23:45:15 +0530

    policy deletion

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -46,6 +46,8 @@ export default function DashboardPage() {
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(null)
 
+  const [deleteModal, setDeleteModal] = useState({ open: false, id: null });
+
   const [query, setQuery] = useState("");
 
   const router = useRouter();
@@ -73,6 +75,21 @@ export default function DashboardPage() {
     }
   };
 
+  const handleDelete = async () => {
+    if (!deleteModal.id) return;
+
+    try {
+      await AXIOS_INSTANCE.delete(`api/policies/actions/${deleteModal.id}/`);
+      toast.success("Policy deleted successfully");
+
+      setPolicies((prev) => prev.filter((p) => p.id !== deleteModal.id));
+
+      setDeleteModal({ open: false, id: null });
+    } catch (err) {
+      console.error(err);
+      toast.error("Failed to delete policy");
+    }
+  };
   useEffect(() => {
     fetchPolicies(1, "");
   }, []);
@@ -205,12 +222,15 @@ export default function DashboardPage() {
                       {/* Actions */}
                       <td className="px-4 py-4 align-top">
                         <div className="flex gap-2 flex-wrap">
-                          <button 
-                          onClick={() => router.push(`dashboard/edit/${policy.id}`)}
-                          className="text-xs bg-blue-100 text-blue-800 px-2 py-0.5 rounded hover:underline cursor-pointer">
+                          <button
+                            onClick={() => router.push(`dashboard/edit/${policy.id}`)}
+                            className="text-xs bg-blue-100 text-blue-800 px-2 py-0.5 rounded hover:underline cursor-pointer">
                             Edit
                           </button>
-                          <button className="text-xs bg-red-100 text-red-800 px-2 py-0.5 rounded hover:underline cursor-pointer">
+                          <button
+                            onClick={() =>
+                              setDeleteModal({ open: true, id: policy.id })
+                            } className="text-xs bg-red-100 text-red-800 px-2 py-0.5 rounded hover:underline cursor-pointer">
                             Delete
                           </button>
                         </div>
@@ -234,6 +254,35 @@ export default function DashboardPage() {
           buttonColor='bg-slate-500'
         />)}
       </main>
+
+      {deleteModal.open && (
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50">
+          <div className="bg-white rounded-xl shadow-xl p-6 w-120 text-center relative animate-fadeIn">
+            <h2 className="text-lg font-semibold text-gray-800 mb-3">
+              Confirm Deletion
+            </h2>
+            <p className="text-sm text-gray-600 mb-5">
+              Are you sure you want to delete this policy? This action cannot be undone.
+            </p>
+
+            <div className="flex justify-center gap-3">
+              <button
+                onClick={() => setDeleteModal({ open: false, id: null })}
+                className="px-4 py-2 cursor-pointer bg-gray-200 rounded-lg text-sm hover:bg-gray-300 transition"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDelete}
+                className="px-4 py-2 bg-red-600 cursor-pointer text-white rounded-lg text-sm hover:bg-red-700 transition shadow-sm"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
